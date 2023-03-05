908b6430b6f8e5119a88db3679e45d497d8cf1ae
vasanthmn1 <mnvasanth4@gmail.com>
2023-03-05 19:16:36 +0530

    ok

--- src/pages/Sliderbar.js
+++ src/pages/Sliderbar.js
@@ -5,7 +5,7 @@ const Sliderbar = () => {
     return (
         <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a className="sidebar-brand d-flex align-items-center justify-content-center">
                 <div className="sidebar-brand-icon rotate-n-15">
                     <i className="fas fa-laugh-wink"></i>
                 </div>
